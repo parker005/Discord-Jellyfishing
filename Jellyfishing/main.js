6cf4094e6339a72cefbc5102437ec6fd8c6d764a
parker <90803531+parker005@users.noreply.github.com>
2022-10-09 22:56:17 -0400

    Update main.js

--- Jellyfishing/main.js
+++ Jellyfishing/main.js
@@ -1,25 +1,44 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, UserFlags, SelectMenuOptionBuilder, Collection, codeBlock } = require('discord.js');
 const { EmbedBuilder } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-const fs = require('fs')
+const Sequelize = require('sequelize');
+const SQLite = require('better-sqlite3')
+const sql = SQLite('./database.sqlite')
+const sequelize = new Sequelize('database', 'user', 'password', {
+    host: 'localhost',
+    dialect: 'sqlite',
+    logging: console.log,
+    storage: 'database.sqlite'
+});
+
+
+const Tags = sequelize.define('tags', {
+    user: {
+        type: Sequelize.STRING,
+        unique: true,
+    },
+    catches: {
+        type: Sequelize.INTEGER,
+        defaultValue: 0,
+        allowNull: false,
+    },
+});
+
+
 
 client.on('ready', () => {
+    Tags.sync();
     console.log(client.user.tag)
 });
 
+
+const sleep = ms => new Promise(res => setTimeout(res, ms));
+
 {var smack = 0};
-
-function add(){ 
-    smack = smack + 1;
-};
-
-function sub(){
-    smack = smack - 1;
-}
 
 
 async function lenny_spawn() {
-    const channel = await client.channels.fetch('917926628561150046')
+    const channel = await client.channels.fetch('1020187811594391622')
     const bobEmbed = new EmbedBuilder()
         .setColor(0x0099FF)
         .setTitle("A WILD JELLYFISH APPPEARS")
@@ -27,7 +46,10 @@ async function lenny_spawn() {
 
     channel.send({ embeds: [bobEmbed] });
 
-    add()
+    smack = 1
+    console.log(smack)
+    await sleep(20*1000);
+    smack=0
     console.log(smack)
 };
 
@@ -41,71 +63,76 @@ client.on('interactionCreate', async interaction => {
 client.on('interactionCreate', async interaction => {
     const { commandName } = interaction;
     if (commandName ===  'catch') {
+        if (smack === 0) {
+            await interaction.reply("No jellyfish here")
+        }
+
         if (smack === 1) {
             const bobEmbed = new EmbedBuilder()
                 .setColor(0x0099FF)
                 .setTitle('Caught!')
 
-            var catPath = "./db.json"
-            var catRead = fs.readFileSync(catPath);
-            var catFile = JSON.parse(catRead);
-            var userId = `${interaction.user}`
+            await interaction.reply({ embeds: [bobEmbed] });
+            smack = 0
+            console.log(smack)
 
-            if (!catFile[userId]) {
-                catFile[userId] = {catches: 1};
-                fs.writeFileSync(catPath, JSON.stringify(catFile, null, 2))
 
-            } else {
-                console.log(catFile[userId].catches)
-                var catVar = Number(catFile[userId].catches);
-                newVar = catVar + 1;
-                
-                console.log(newVar)
-                catFile[userId] = {catches: newVar};
-                fs.writeFileSync(catPath, JSON.stringify(catFile, null, 2));
+            var tagUser = `${interaction.user}`
 
-                console.log(`changed users catch count to ${newVar}`)
+            try {
+                const tag = await Tags.create({
+                    user: tagUser,
+                    catches: 1,
+                });
+                return interaction.followUp(`User ${tag.user} now logged into database`);
             }
 
-            await interaction.reply({ embeds: [bobEmbed] });
-            sub()
-            console.log(smack)
+            catch (error) {
+                if (error.name === "SequelizeUniqueConstraintError") {
+                    const tag = await Tags.findOne({ where: {user: tagUser}});
+
+                    if (tag) {
+                        tag.increment('catches')
+                        return interaction.followUp(tagUser + " has " + String(tag.get('catches'))+" catches total")
+                    }
+                    }
+                }
+            }
         }
-        else {
-            await interaction.reply("No jellyfish here")
-        }
+    
+    if (commandName === 'leaderboard') {
+        board = []
+        const top10 = await sql.prepare("SELECT * FROM tags ORDER BY catches DESC LIMIT 10;").all();
+        top10.map(({ user, catches }) => {
+            board.push(`${user} ${catches}`)
+        });
+        board = board.toString();
+        board = board.replace(",", "\n")
+
+        const embed = new EmbedBuilder()
+            .setTitle("Leaderboard")
+            .setColor(0x0099FF)
+            .addFields({ name: '------------------', value: board});
+        return interaction.reply({ embeds: [embed] });
+        
     }
 
-    if (commandName === 'leaderboard') {
-        var catPath = "./db.json"
-        var catRead = fs.readFileSync(catPath);
-        var data = JSON.parse(catRead);
-        sorted = []
-        sorted.push(data);
-        function sortData(){
-            sorted.sort(function(a, b) {
-                return b-a
-            });
-        }
-        
-        sortData()
-        
-        console.log(data)
-        
-        const bobEmbed = new EmbedBuilder()
+    if (commandName === 'lennyhelp') {
+        const embed = new EmbedBuilder()
+            .setTitle("Command List")
             .setColor(0x0099FF)
-            .setTitle('Leaderboard')
-            .setDescription(data)
-        
-            await interaction.reply({ embeds: [bobEmbed]})
+            .setDescription("~ /lenny *a simple command that replies hi\n~ /catch *when you see a jellyfish enter this command to catch it*\n~ /leaderboard *replies with a leaderboard of catches in the server*")
+
+        return interaction.reply({ embeds: [embed]})
     }
-})
+
+});
 
 
 function spawn_lenny() {
     lenny_spawn()
 }
 
-setInterval(spawn_lenny, 30*1000);
+setInterval(spawn_lenny, 650*1000);
 
-client.login('MTAyNTYxNTY0NDkyMzgwOTg0Mw.G_WcbZ.0oQxRzqT70WSrbfIqUWeDSxFzzp_EgaTsMKJK8')
+client.login('')
